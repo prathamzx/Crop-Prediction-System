73c61edd2b1bafe6fcd58a4bfd453404f5fc203d
KaranSharma-gawds <karansharma232ds@gmail.com>
2018-10-28 01:24:13 +0530

    adding comments in train.py

--- Qualify_or_ddos git/train.py
+++ Qualify_or_ddos git/train.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 
+# Defining the DataSet preprocessing Function
 def dataset(area):
     data = pd.read_csv("C:\\datafiles\\crop_production.csv")
     del data["District_Name"]
@@ -14,10 +15,11 @@ def dataset(area):
     X = X[:242361]
     X.shape
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
-
+    # Instantiating the Classifier
     clf =  GradientBoostingRegressor()
-
+    # Fitting the model
     clf.fit(X_train,y_train)
+    # Predicting the results
     z=clf.predict([[area,2000]])
     print(z)
     return(z)
